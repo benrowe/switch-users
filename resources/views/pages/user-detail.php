d1a3a10698646230c47d1d2e254c1136b774ff0e
Ben Rowe <ben.rowe.83@gmail.com>
2017-03-05 16:39:39 +1100

    Update the user detail view to include modal wrapping

--- resources/views/pages/user-detail.php
+++ resources/views/pages/user-detail.php
@@ -1,24 +1,33 @@
-<dl>
-    <dt>User ID</dt>
-    <dd><?=$model->user_id?></dd>
-    <dt>Name</dt>
-    <dd><?=$model->name?></dd>
-    <dt>ID</dt>
-    <dd><a href="mailto:<?=$model->email?>"><?=$model->email?></a></dd>
-    <dt>ID</dt>
-    <dd><?=$model->role?></dd>
-    <dt>ID</dt>
-    <dd><?=$model->department?></dd>
-    <dt>ID</dt>
-    <dd><?=$model->dob?></dd>
-    <dt>ID</dt>
-    <dd><?=$model->street_address_1?><br><?=$model->street_address_2?></dd>
-    <dt>ID</dt>
-    <dd><?=$model->suburb?></dd>
-    <dt>ID</dt>
-    <dd><?=$model->state?></dd>
-    <dt>ID</dt>
-    <dd><?=$model->postcode?></dd>
-    <dt>ID</dt>
-    <dd><?=$model->country?></dd>
-</dl>
+<div class="modal-header">
+  <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+  <h4 class="modal-title" id="myModalLabel">Profile: <?=$model->name?> (id #<?=$model->user_id?>)</h4>
+</div>
+<div class="modal-body">
+    <dl>
+        <dt>User ID</dt>
+        <dd><?=$model->user_id?></dd>
+        <dt>Name</dt>
+        <dd><?=$model->name?></dd>
+        <dt>Email</dt>
+        <dd><a href="mailto:<?=$model->email?>"><?=$model->email?></a></dd>
+        <dt>Role</dt>
+        <dd><?=$model->role?></dd>
+        <dt>Department</dt>
+        <dd><?=$model->department?></dd>
+        <dt>Date of Birth</dt>
+        <dd><?=$model->dob?> (<?=$model->ageInYears?> years old)</dd>
+        <dt>Street Address</dt>
+        <dd><?=$model->street_address_1?><br><?=$model->street_address_2?></dd>
+        <dt>Suburb</dt>
+        <dd><?=$model->suburb?></dd>
+        <dt>State</dt>
+        <dd><?=$model->state?></dd>
+        <dt>Postcode</dt>
+        <dd><?=$model->postcode?></dd>
+        <dt>Country</dt>
+        <dd><?=$model->country?></dd>
+    </dl>
+</div>
+<div class="modal-footer">
+  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+</div>
