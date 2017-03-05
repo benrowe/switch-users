6196dc2ea721f9d7c12dbbff47ae135763442216
Ben Rowe <ben.rowe.83@gmail.com>
2017-03-05 13:37:28 +1100

    Add a test controller call

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -5,6 +5,4 @@ use App\Http\Controllers\UserController;
 /**
  * @var \League\Route\RouteCollection $route;
  */
-$route->addRoute('GET', '/', function () {
-    return '/HI';
-});
+$route->map('GET', '/', UserController::class.'::index');
