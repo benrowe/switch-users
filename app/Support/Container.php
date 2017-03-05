4e73991c7a83cd1368da6580846b953cf4a4ed82
Ben Rowe <ben.rowe.83@gmail.com>
2017-03-05 13:19:25 +1100

    Add request handling through routes

--- app/Support/Container.php
+++ app/Support/Container.php
@@ -5,6 +5,9 @@ namespace App\Support;
 use App\Exceptions\InvalidCallException;
 use League\Container\Container as DiContainer;
 use League\Container\ReflectionContainer;
+use League\Route\RouteCollection;
+use Symfony\Bridge\PsrHttpMessage\Factory\DiactorosFactory;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Application Container
@@ -67,6 +70,18 @@ class Container
     }
 
     /**
+     * Run the http application
+     * @param  string $routePath location of the routes file
+     */
+    public function run($routePath)
+    {
+        $route = $this->getRouteCollection($routePath);
+        $response = $this->dispatchRoute($route);
+
+        $this->get('emitter')->emit($response);
+    }
+
+    /**
      * Forward any undefined methods to the DI container
      * This will enable the application instance to register injectable services
      *
@@ -82,5 +97,48 @@ class Container
         }
 
         return call_user_func_array([$this->dependency, $method], $params);
+    }
+
+    private function getRouteCollection($path): RouteCollection
+    {
+        $route = new RouteCollection($this->dependency);
+
+        require $this->root().$path;
+
+        return $route;
+    }
+
+    private function dispatchRoute(RouteCollection $route)
+    {
+        $psr7 = new DiactorosFactory();
+        $response = $route->dispatch(
+            $psr7->createRequest($this->get('request')),
+            $psr7->createResponse($this->get('response'))
+        );
+
+        return $this->handleResponseTypes($response);
+    }
+
+    /**
+     * @param ResponseInterface|string|array $response
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    private function handleResponseTypes($response)
+    {
+        // string
+        if (is_string($response)) {
+            return (new DiactorosFactory)->createResponse(new Response($response));
+        }
+        // array
+        if (is_array($response)) {
+            return (new DiactorosFactory)->createResponse(
+                new Response(json_encode($response), 200, ['content-type' => 'application/json'])
+            );
+        }
+        // catch unhandled responses
+        if (!($response instanceof ResponseInterface)) {
+            throw new \RuntimeException("Route response is unsupported");
+        }
+        return $response;
     }
 }
--- public/index.php
+++ public/index.php
@@ -1,3 +1,5 @@
 <?php
 
 $app = require_once realpath(__DIR__.'/../bootstrap.php');
+
+$app->run('app/Http/routes.php');
