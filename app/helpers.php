ce1c5d10bb9e6dc42bf1cf1f8f738938636c1fde
Ben Rowe <ben.rowe.83@gmail.com>
2017-03-05 12:10:14 +1100

    Add global \App\path() to get absolute path relative to project root.

--- app/helpers.php
+++ app/helpers.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App;
+
 /**
  * This file contains global functions that provide quick and easy access to
  * this applications services,
@@ -8,3 +10,23 @@
  *
  * global convenience functions
  */
+
+/**
+ * Get an instance of the application container
+ *
+ * @return \App\Support\Container
+ */
+function app()
+{
+    return \App\Support\Container::instance();
+}
+
+function path($relativePath = null)
+{
+    $path = app()->root();
+    if ($relativePath !== null) {
+        $path .= trim($relativePath, '/');
+    }
+
+    return $path;
+}
